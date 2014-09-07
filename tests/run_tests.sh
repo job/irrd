4e99500d90327b6f109b63d0f98a57514bae4bfe
Job Snijders <job@instituut.net>
2014-09-07 18:27:47 +0000

    rework testing script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,9 +4,9 @@ set -o errexit
 function run_tests {
     mkdir -p /tmp/irr/pgp_dir /tmp/irr/log
     sudo irrd -f irrd.conf &
-    sleep 1
+    sleep 2
     tests_round_1
-    tests_with_hiding
+    tests_with_pwhash_hiding
     killirrd
 }
 
@@ -16,7 +16,7 @@ function reload_irrd {
         sleep 2 
         echo -e "reload sampledb\n\r" 
         sleep 2 ) | telnet localhost 5673 
-    sleep 1
+    sleep 2
     whois -h localhost MAINT-TEST
 }
 
@@ -29,7 +29,7 @@ function run {
         exit 2
     fi
     if [[ ${haystack} = *${needle}* ]]; then
-        echo "OK indicator found"
+        echo "OK indicator \"${needle}\" found"
     else
         echo "Could not find success indicator \"${needle}\" in this test, aborting"
         exit 2
@@ -39,28 +39,28 @@ function run {
 function test_001 {
     echo "INFO: testing object creation authenticated with UNIX crypt password"
     irr_rpsl_submit -v -f irrd.conf -s sampledb -x < test_001_upload_certif_with_unixcrypt.txt
-    sleep 1
+    sleep 2
     whois -h localhost TESTUSER-sampledb
 }
 
 function test_002 {
     echo "INFO: testing object modification authenticated with MD5 password"
     irr_rpsl_submit -v -f irrd.conf -s sampledb -x < test_002_modify_person_with_md5.txt
-    sleep 1
+    sleep 2
     whois -h localhost TESTUSER-sampledb
 }
 
 function test_003 {
     echo "INFO: testing object modification authenticated with GPG signature"
     irr_rpsl_submit -v -f irrd.conf -s sampledb -x < test_003_modify_person_with_gpg.txt
-    sleep 1
+    sleep 2
     whois -h localhost TESTUSER-sampledb
 }
 
 function test_004 {
     echo "INFO: testing heasely's i6 patch"
     irr_rpsl_submit -v -f irrd.conf -s sampledb -x < test_004_test_heasely_i6.txt
-    sleep 1
+    sleep 2
     whois -h localhost AS1:AS-ALL
 }
 
@@ -77,30 +77,48 @@ function tests_round_1 {
     run test_003 "REPLACE OK: " "FAILED"
     echo; echo
     run test_004 "OK" "FAIL"
+    echo; echo
+    # the following essentially is test_005
+    run show_irrd_config "sampledb" "FAIL"
 
 #    echo '!i6AS1:AS-ALL' | telnet localhost 43
 }
 
-function tests_with_hiding {
+function tests_with_pwhash_hiding {
+    echo "INFO: applying ACL for password hash hiding to the running daemon"
     (   echo "admin_citesting"
-    sleep 1
-    echo "config"
-    sleep 1
-    echo "access-list 99 deny all"
-    sleep 1
-    echo "irr_database sampledb cryptpw-access 99"
-    sleep 1
-    echo "exit"; sleep 1
-    echo "exit"; sleep 1 ) | telnet localhost 5673 || echo
+    sleep 2
+    echo -e "config\n\r"
+    sleep 2
+    echo -e "access-list 99 permit ::1/128\n\r"
+    sleep 2
+    echo -e "access-list 99 permit 127.0.0.0/8\n\r"
+    sleep 2
+    echo -e "access-list 99 deny all\n\r"
+    sleep 2
+    echo -e "irr_database sampledb cryptpw-access 99\n\r"
+    sleep 2
+    echo -e "exit\n\r"; sleep 2
+    echo -e "exit\n\r"; sleep 2 ) | telnet localhost 5673 || echo
     tests_round_1
 }
+
+function show_irrd_config {
+    (   echo "admin_citesting"
+    sleep 2
+    echo -e "show config\n\r"
+    sleep 2
+    echo -e "exit\n\r";
+    sleep 2 ) | telnet localhost 5673 || echo
+}
+
 
 function rebootirrd {
     # reboot
     (   echo "admin_citesting"
         sleep 2
         echo "reboot"
-        sleep 1 ) | telnet localhost 5673 || echo
+        sleep 2 ) | telnet localhost 5673 || echo
     sleep 5
 }
 
@@ -109,7 +127,7 @@ function killirrd {
     (   echo "admin_citesting"
         sleep 2
         echo "kill"
-        sleep 1 ) | telnet localhost 5673 || echo
+        sleep 2 ) | telnet localhost 5673 || echo
 }
 
 run_tests
